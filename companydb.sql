688aba1700fc5dcbf368fd4934480a2a91e3f4a2
Diem Nguyen <diiemnguyen@yahoo.com>
2013-10-02 16:50:05 -0700

    changes sql

--- companydb.sql
+++ companydb.sql
@@ -7,8 +7,8 @@ USE companydb;
 /* Table 1: company */
 DROP TABLE IF EXISTS company;
 CREATE TABLE company
-(dept_ID INT NOT NULL,
- dept_Name VARCHAR (50) NOT NULL,
+(dept_ID INT,
+ dept_Name VARCHAR (50),
  dept_Manager VARCHAR (100),
  PRIMARY KEY(dept_ID)
 );
@@ -17,13 +17,13 @@ CREATE TABLE company
 /* Table 2: employees */
 DROP TABLE IF EXISTS employees;
 CREATE TABLE employees
-(empl_ID INT NOT NULL,
- dept_ID INT NOT NULL,
- empl_Name VARCHAR (100) NOT NULL,
- empl_DOB FORMAT(DATE, '00-00-0000') NOT NULL, 
- empl_Email VARCHAR (100) NOT NULL, 
- empl_Position VARCHAR (50) NOT NULL, 
- empl_Salary DOUBLE NOT NULL,
+(empl_ID INT,
+ dept_ID INT,
+ empl_Name VARCHAR (100),
+ empl_DOB DATE, 
+ empl_Email VARCHAR (100), 
+ empl_Position VARCHAR (50), 
+ empl_Salary DOUBLE,
  PRIMARY KEY(empl_ID,dept_ID)
 );
 
@@ -43,8 +43,8 @@ CREATE TABLE sale_transactions
 /* Table 4: products */
 DROP TABLE IF EXISTS products;
 CREATE TABLE products
-(prod_ID INT NOT NULL,
- prod_NAME VARCHAR(30) NOT NULL,
+(prod_ID INT,
+ prod_NAME VARCHAR(30),
  PRIMARY KEY (prod_ID)
 ); 
 
@@ -52,12 +52,12 @@ CREATE TABLE products
 /* Table 5: product_lists */
 DROP TABLE IF EXISTS product_lists;
 CREATE TABLE product_lists
-(prod_ID INT NOT NULL,
- prod_No INT NOT NULL,
- prod_Title VARCHAR (200) NOT NULL,
- prod_Case_Qty INT NOT NULL,
+(prod_ID INT,
+ prod_No INT,
+ prod_Title VARCHAR (200),
+ prod_Case_Qty INT,
  prod_Case_Weight INT,
- prod_Case_Price DOUBLE NOT NULL,
+ prod_Case_Price DOUBLE,
  PRIMARY KEY(prod_ID,prod_No,prod_Title)
 ); 
 
@@ -65,10 +65,9 @@ CREATE TABLE product_lists
 /* Table 6: customers */
 DROP TABLE IF EXISTS customers;
 CREATE TABLE customers
-(cust_ID INT NOT NULL,
+(cust_ID INT,
  cust_Name VARCHAR (100),
- cust_Email VARCHAR (100) NOT NULL,
- cust_Date NOT NULL DEFAULT FORMAT(CURDATE(),'MM-DD-YYYY'),
+ cust_Email VARCHAR (100),
  PRIMARY KEY(cust_ID)
 );
 
@@ -76,11 +75,11 @@ CREATE TABLE customers
 /* Table 7: customer_orders */
 DROP TABLE IF EXISTS customer_orders;
 CREATE TABLE customer_orders
-(cust_ID INT NOT NULL,
- prod_ID INT NOT NULL,
- cust_Total_Order INT NOT NULL,
- cust_Year_Order INT NOT NULL,
- cust_Total_Amount DOUBLE NOT NULL,
+(cust_ID INT,
+ prod_ID INT,
+ cust_Total_Order INT,
+ cust_Year_Order INT,
+ cust_Total_Amount DOUBLE
 );
 
 
