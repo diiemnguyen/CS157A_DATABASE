731c66975e88a84d886da3393348b492944cd511
Diem Nguyen <diiemnguyen@yahoo.com>
2013-10-28 12:47:09 -0700

    update java classes

---
+++ src/LookUpTable.java
@@ -0,0 +1,166 @@
+/**
+ * Group: 479
+ * Trinh,Qui Thu (quibie7@gmail.com)
+ * Nguyen,Diem Ngoc (diiemnguyen@yahoo.com)
+ * Le,Thinh Minh (tmle04@yahoo.com)
+ */
+
+// Display the contents of the inventory table in the
+// companydb database.
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.SQLException;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JTable;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.Box;
+
+public class LookUpTable extends JFrame 
+{
+   // JDBC driver, database URL, username and password
+   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+   static final String DB_URL = "jdbc:mysql://localhost/companydb";
+   static final String USER= "root";
+   static final String PASS= "Passw0rd";
+   
+   // default query retrieves all data from authors table
+   static final String DEFAULT_QUERY = "SELECT * FROM orders";
+   
+   private LookUpTableModel tableModel;
+   private JTextArea queryArea;
+   
+   // create LookUpTable and GUI
+   public LookUpTable() 
+   {   
+      super( "Administration" );
+        
+      // create LookUpTable and display database table
+      try 
+      {
+         // create TableModel for results of query SELECT * FROM orders
+         tableModel = new LookUpTableModel( JDBC_DRIVER, DB_URL, 
+            USER, PASS, DEFAULT_QUERY );
+
+         // set up JTextArea in which user types queries
+         queryArea = new JTextArea( DEFAULT_QUERY, 3, 100 );
+         queryArea.setWrapStyleWord( true );
+         queryArea.setLineWrap( true );
+         
+         JScrollPane scrollPane = new JScrollPane( queryArea,
+            ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, 
+            ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER );
+         
+         // set up JButton for executing queries
+         JButton submitButton = new JButton( "Execute" );
+
+         // create Box to manage placement of queryArea and 
+         // submitButton in GUI
+         Box box = Box.createHorizontalBox();
+         box.add( scrollPane );
+         box.add( submitButton );
+
+         // create JTable delegate for tableModel 
+         JTable resultTable = new JTable( tableModel );
+         
+         // place GUI components on content pane
+         add( box, BorderLayout.NORTH );
+         add( new JScrollPane( resultTable ), BorderLayout.CENTER );
+
+         // create event listener for submitButton
+         submitButton.addActionListener( 
+         
+            new ActionListener() 
+            {
+               // pass query to table model
+               public void actionPerformed( ActionEvent event )
+               {
+                  // perform a new query
+                  try 
+                  {
+                     tableModel.setQuery( queryArea.getText() );
+                  } // end try
+                  catch ( SQLException sqlException ) 
+                  {
+                     JOptionPane.showMessageDialog( null, 
+                        sqlException.getMessage(), "Database error", 
+                        JOptionPane.ERROR_MESSAGE );
+                     
+                     // try to recover from invalid user query 
+                     // by executing default query
+                     try 
+                     {
+                        tableModel.setQuery( DEFAULT_QUERY );
+                        queryArea.setText( DEFAULT_QUERY );
+                     } // end try
+                     catch ( SQLException sqlException2 ) 
+                     {
+                        JOptionPane.showMessageDialog( null, 
+                           sqlException2.getMessage(), "Database error", 
+                           JOptionPane.ERROR_MESSAGE );
+         
+                        // ensure database connection is closed
+                        tableModel.disconnectFromDatabase();
+         
+                        System.exit( 1 ); // terminate application
+                     } // end inner catch                   
+                  } // end outer catch
+               } // end actionPerformed
+            } // end ActionListener inner class          
+         ); // end call to addActionListener
+
+         setSize( 1000, 500 ); 	// set window size
+         setVisible( true );	// display window  
+      } // end try
+      catch ( ClassNotFoundException classNotFound ) 
+      {
+         JOptionPane.showMessageDialog( null, 
+            "MySQL driver not found", "Driver not found",
+            JOptionPane.ERROR_MESSAGE );
+         
+         System.exit( 1 ); // terminate application
+      } // end catch
+      catch ( SQLException sqlException ) 
+      {
+         JOptionPane.showMessageDialog( null, sqlException.getMessage(), 
+            "Database error", JOptionPane.ERROR_MESSAGE );
+               
+         // ensure database connection is closed
+         tableModel.disconnectFromDatabase();
+         
+         System.exit( 1 );   // terminate application
+      } // end catch
+      
+      // dispose of window when user quits application (this overrides
+      // the default of HIDE_ON_CLOSE)
+      setDefaultCloseOperation( DISPOSE_ON_CLOSE );
+      
+      // ensure database connection is closed when user quits application
+      addWindowListener(
+      
+         new WindowAdapter() 
+         {
+            // disconnect from database and exit when window has closed
+            public void windowClosed( WindowEvent event )
+            {
+               tableModel.disconnectFromDatabase();
+               System.exit( 0 );
+            } // end method windowClosed
+         } // end WindowAdapter inner class
+      ); // end call to addWindowListener
+   } // end LookUpTable constructor
+   
+   // execute application
+   public static void main( String args[] ) 
+   {
+      new LookUpTable();     
+   } // end main
+   
+} // end class LookUpTable
+
---
+++ src/LookUpTableModel.java
@@ -0,0 +1,200 @@
+/**
+ * Group: 479
+ * Trinh,Qui Thu (quibie7@gmail.com)
+ * Nguyen,Diem Ngoc (diiemnguyen@yahoo.com)
+ * Le,Thinh Minh (tmle04@yahoo.com)
+ */
+
+
+//A TableModel that supplies ResultSet data to a JTable.
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import javax.swing.table.AbstractTableModel;
+
+// ResultSet rows and columns are counted from 1 and JTable 
+// rows and columns are counted from 0. When processing 
+// ResultSet rows or columns for use in a JTable, it is 
+// necessary to add 1 to the row or column number to manipulate
+// the appropriate ResultSet column (i.e., JTable column 0 is 
+// ResultSet column 1 and JTable row 0 is ResultSet row 1).
+public class LookUpTableModel extends AbstractTableModel 
+{
+   private Connection connection;
+   private Statement statement;
+   private ResultSet rs;
+   private ResultSetMetaData rsMetaData;
+   private int numRow;
+
+   // keep track of database connection status
+   private boolean connectDB = false;
+   
+   // constructor initializes rs and obtains its meta data object;
+   // determines number of rows
+   public LookUpTableModel( String driver, String url, 
+      String username, String password, String query ) 
+      throws SQLException, ClassNotFoundException
+   {         
+      // load database driver class
+      Class.forName( driver );
+
+      // connect to database
+      connection = DriverManager.getConnection( url, username, password );
+
+      // create Statement to query database
+      statement = connection.createStatement( 
+         ResultSet.TYPE_SCROLL_INSENSITIVE,
+         ResultSet.CONCUR_READ_ONLY );
+
+      // update database connection status
+      connectDB = true;
+
+      // set query and execute it
+      setQuery( query );
+   } // end constructor LookUpTableModel
+
+   // get class that represents column type
+   public Class getColumnClass( int column ) throws IllegalStateException
+   {
+      // ensure database connection is available
+      if ( !connectDB ) 
+         throw new IllegalStateException( "Not Connected to Database" );
+
+      // determine Java class of column
+      try 
+      {
+         String className = rsMetaData.getColumnClassName( column + 1 );
+         
+         // return Class object that represents className
+         return Class.forName( className );
+      } // end try
+      catch ( Exception exception ) 
+      {
+         exception.printStackTrace();
+      } // end catch
+      
+      return Object.class; // if problems occur above, assume type Object
+   } // end method getColumnClass
+
+   // get number of columns in ResultSet
+   public int getColumnCount() throws IllegalStateException
+   {   
+      // ensure database connection is available
+      if ( !connectDB ) 
+         throw new IllegalStateException( "Not Connected to Database" );
+
+      // determine number of columns
+      try 
+      {
+         return rsMetaData.getColumnCount(); 
+      } // end try
+      catch ( SQLException sqlException ) 
+      {
+         sqlException.printStackTrace();
+      } // end catch
+      
+      return 0; // if problems occur above, return 0 for number of columns
+   } // end method getColumnCount
+
+   // get name of a particular column in ResultSet
+   public String getColumnName( int column ) throws IllegalStateException
+   {    
+      // ensure database connection is available
+      if ( !connectDB ) 
+         throw new IllegalStateException( "Not Connected to Database" );
+
+      // determine column name
+      try 
+      {
+         return rsMetaData.getColumnName( column + 1 );  
+      } // end try
+      catch ( SQLException sqlException ) 
+      {
+         sqlException.printStackTrace();
+      } // end catch
+      
+      return ""; // if problems, return empty string for column name
+   } // end method getColumnName
+
+   // return number of rows in ResultSet
+   public int getRowCount() throws IllegalStateException
+   {      
+      // ensure database connection is available
+      if ( !connectDB ) 
+         throw new IllegalStateException( "Not Connected to Database" );
+ 
+      return numRow;
+   } // end method getRowCount
+
+   // obtain value in particular row and column
+   public Object getValueAt( int row, int column ) 
+      throws IllegalStateException
+   {
+      // ensure database connection is available
+      if ( !connectDB ) 
+         throw new IllegalStateException( "Not Connected to Database" );
+
+      // obtain a value at specified ResultSet row and column
+      try 
+      {
+         rs.absolute( row + 1 );
+         return rs.getObject( column + 1 );
+      } // end try
+      catch ( SQLException sqlException ) 
+      {
+         sqlException.printStackTrace();
+      } // end catch
+      
+      return ""; // if problems, return empty string object
+   } // end method getValueAt
+   
+   // set new database query string
+   public void setQuery( String query ) throws SQLException, IllegalStateException 
+   {
+      // ensure database connection is available
+      if ( !connectDB ) 
+         throw new IllegalStateException( "Not Connected to Database" );
+
+      // specify query and execute it
+      rs = statement.executeQuery( query );
+
+      // obtain meta data for ResultSet
+      rsMetaData = rs.getMetaData();
+
+      // determine number of rows in ResultSet
+      rs.last();             	// move to last row
+      numRow = rs.getRow();  	// get row number      
+      
+      // notify JTable that model has changed
+      fireTableStructureChanged();
+   } // end method setQuery
+
+   // close Statement and Connection               
+   public void disconnectFromDatabase()            
+   {              
+      if ( !connectDB )                  
+         return;
+
+      // close Statement and Connection            
+      try                                          
+      {                                            
+         statement.close();                        
+         connection.close();                       
+      } // end try                                 
+      catch ( SQLException sqlException )          
+      {                                            
+         sqlException.printStackTrace();           
+      } // end catch                               
+      finally  // update database connection status
+      {                                            
+         connectDB = false;              
+      } // end finally                             
+   } // end method disconnectFromDatabase     
+   
+}  // end class LookUpTableModel
+
+
